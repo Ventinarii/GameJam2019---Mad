d460426726189a9e1c422b4d1c5c7717fe77d591
s17149-PJ <64791402+s17149-PJ@users.noreply.github.com>
2020-06-29 18:13:08 +0200

    Initial comit
    Uploaded project fomr my private archive

---
+++ GameJam2019s16422/Assets/Manager/GameMaster.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public GameObject Hero;
+    public static HeroController HeroController;
+    public static List<GameObject> Enemies = new List<GameObject>();
+    
+    void Start()
+    {
+        HeroController = Hero.GetComponent<HeroController>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+
+}
